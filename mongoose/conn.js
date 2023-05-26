c80d6efa1dd4752c4c08c055962c49dcf3b9fd8e
Milind <milibhatnagar2002@gmail.com>
2023-05-26 12:34:59 +0530

    Removed exposed Uri

--- mongoose/conn.js
+++ mongoose/conn.js
@@ -1,2 +1,2 @@
 import mongoose from "mongoose";
-mongoose.connect("mongodb+srv://root:My_Milind_MongoDB%402024@cluster0.bfakyrg.mongodb.net/diary", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("/Your mongodb uri here", { useNewUrlParser: true, useUnifiedTopology: true });
